1c5a0f1dc47ba0d51b21bbbf15127d199ec358f0
Michael Jackson <mjijackson@gmail.com>
2016-09-13 08:10:06 -0700

    Use global React in UMD build

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,15 @@ module.exports = {
     libraryTarget: 'umd'
   },
 
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react'
+    }
+  },
+
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
