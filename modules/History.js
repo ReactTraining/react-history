5cec5ba95a308d444d097d5fce966b4b0eb4611d
Ryan Florence <rpflorence@gmail.com>
2016-10-06 16:33:09 -0700

    remove support for changing history options dynamically
    seems weird anyway, sorry about that.

--- modules/History.js
+++ modules/History.js
@@ -17,36 +17,10 @@ class History extends React.Component {
     }
   }
 
-  setupHistory(props) {
-    const { createHistory, historyOptions } = props
+  componentWillMount() {
+    const { createHistory, historyOptions } = this.props
     this.history = createHistory(historyOptions)
     this.unlisten = this.history.listen(() => this.forceUpdate())
-  }
-
-  componentWillMount() {
-    this.setupHistory(this.props)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    const prevHistoryOptions = this.props.historyOptions
-    const nextHistoryOptions = nextProps.historyOptions
-
-    // TODO: Each type of history should have the ability to determine
-    // whether a prop change requires creation of a new history object.
-    // For example, <MemoryHistory> doesn't need a new history instance
-    // when the initialEntries prop changes.
-    let changed = false
-    for (const key in nextHistoryOptions) {
-      if (nextHistoryOptions[key] !== prevHistoryOptions[key]) {
-        changed = true
-        break
-      }
-    }
-
-    if (changed) {
-      this.unlisten()
-      this.setupHistory(nextProps)
-    }
   }
 
   componentWillUnmount() {
