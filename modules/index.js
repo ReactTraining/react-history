ee2ecc5ed5440ced1aea0b8847809aee59d7f844
Ryan Florence <rpflorence@gmail.com>
2016-10-06 20:54:51 -0700

    ControlledHistory -> ControlledHistoryExperimental

--- modules/index.js
+++ modules/index.js
@@ -1,6 +1,7 @@
 export BrowserHistory from './BrowserHistory'
 export HashHistory from './HashHistory'
 export MemoryHistory from './MemoryHistory'
+export ControlledHistoryExperimental from './ControlledHistoryExperimental'
 export Prompt from './Prompt'
 export * from './Actions'
 export { createLocation, locationsAreEqual } from './LocationUtils'
