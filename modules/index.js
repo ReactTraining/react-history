df295a097317c84911bc75891c0ba26ae164b4c2
Ryan Florence <rpflorence@gmail.com>
2016-10-06 21:13:56 -0700

    remove controlled history from exports

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,6 @@
 export BrowserHistory from './BrowserHistory'
 export HashHistory from './HashHistory'
 export MemoryHistory from './MemoryHistory'
-export ControlledHistoryExperimental from './ControlledHistoryExperimental'
 export Prompt from './Prompt'
 export * from './Actions'
 export { createLocation, locationsAreEqual } from './LocationUtils'
