a1b06d3339e5ddd0d4648e045d1c451974331b42
Asa Ayers <asa@asaayers.com>
2016-10-12 09:24:41 -0700

    Update index.js

--- modules/index.js
+++ modules/index.js
@@ -1,7 +1,7 @@
 export BrowserHistory from './BrowserHistory'
 export HashHistory from './HashHistory'
 export MemoryHistory from './MemoryHistory'
-export History from './History
+export History from './History'
 export Prompt from './Prompt'
 export * from './Actions'
 export { createLocation, locationsAreEqual } from './LocationUtils'
