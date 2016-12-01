a5c8b150aeb97b56f722cc1c337a76055bc6837b
Forbes Lindesay <forbes@lindesay.co.uk>
2016-12-01 13:42:38 +0000

    Share as much code as possible between history providers

--- modules/HashHistory.js
+++ modules/HashHistory.js
@@ -1,66 +1,24 @@
-import React, { PropTypes } from 'react'
+import { PropTypes } from 'react'
 import createHashHistory from 'history/createHashHistory'
-import {
-  history as historyType
-} from './PropTypes'
-
+import createHistoryProvider from './createHistoryProvider'
 /**
  * Manages session history using window.location.hash.
  */
-class HashHistory extends React.Component {
-  static propTypes = {
+const HashHistory = createHistoryProvider(
+  {
     basename: PropTypes.string,
     getUserConfirmation: PropTypes.func,
     hashType: PropTypes.oneOf([
       'hashbang',
       'noslash',
       'slash'
-    ]),
-    children: PropTypes.oneOfType([
-      PropTypes.node,
-      PropTypes.func
-    ]).isRequired
-  }
-
-  static childContextTypes = {
-    history: historyType.isRequired
-  }
-
-  getChildContext() {
-    return {
-      history: this.history
-    }
-  }
-
-  componentWillMount() {
-    const { basename, getUserConfirmation, hashType } = this.props
-
-    this.history = createHashHistory({
-      basename,
-      getUserConfirmation,
-      hashType
-    })
-
-    // Do this here so we catch actions in cDM.
-    this.unlisten = this.history.listen(() => this.forceUpdate())
-  }
-
-  componentWillUnmount() {
-    this.unlisten()
-  }
-
-  render() {
-    const { children } = this.props
-
-    if (typeof children !== 'function')
-      return React.Children.only(children)
-
-    return children({
-      action: this.history.action,
-      location: this.history.location,
-      history: this.history
-    })
-  }
-}
+    ])
+  },
+  ({ basename, getUserConfirmation, hashType }) => createHashHistory({
+    basename,
+    getUserConfirmation,
+    hashType
+  })
+)
 
 export default HashHistory
